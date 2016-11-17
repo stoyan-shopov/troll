f0f380610160a0970df1fd643888f797d8976ba5
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-11-17 14:48:01 +0200

    debug instrumentation

--- blackstrike.cxx
+++ blackstrike.cxx
@@ -1,11 +1,17 @@
 #include <QDebug>
+#include <QTime>
 #include "blackstrike.hxx"
+
+#define BLACKSTIRKE_DEBUG	0
 
 void Blackstrike::readAllRegisters()
 {
 QString s;
 QRegExp rx("<<<start>>>(.*)<<<end>>>");
 int pos;
+QTime t;
+
+	t.start();
 	registers.clear();
 	port->write("swdp-scan drop gdb-attach drop\n");
 	port->write(".( <<<start>>>)cr ?regs .( <<<end>>>)cr\n");
@@ -17,11 +23,11 @@ int pos;
 			Util::panic();
 	}
 	while (!s.contains("<<<end>>>"));
-	qDebug() << s;
+	if (BLACKSTIRKE_DEBUG) qDebug() << s;
 	s.replace('\n', "");
 	if (rx.indexIn(s) == -1)
 		Util::panic();
-	qDebug() << "string recognized: " << rx.cap();
+	if (BLACKSTIRKE_DEBUG) qDebug() << "string recognized: " << rx.cap();
 	s = rx.cap(1);
 	rx.setPattern("\\s*(\\S+)");
 	pos = 0;
@@ -31,9 +37,10 @@ int pos;
 		registers.push_back(rx.cap(1).toUInt(& ok, 16));
 		if (!ok)
 			Util::panic();
-		qDebug() << "read value" << registers.back();
+		if (BLACKSTIRKE_DEBUG) qDebug() << "read value" << registers.back();
 		pos += rx.matchedLength();
 	}
+	qDebug() << "target register read took" << t.elapsed() << "milliseconds";
 }
 
 uint32_t Blackstrike::readWord(uint32_t address)
@@ -42,7 +49,9 @@ QString s;
 QRegExp rx("<<<start>>>(.*)<<<end>>>");
 bool ok;
 uint32_t x;
+QTime t;
 
+	t.start();
 	port->write(QString("$%1 ").arg(address, 0, 16).toLocal8Bit());
 	port->write("base @ >r hex .( <<<start>>>) t@ u. .( <<<end>>>) r> base ! cr\n");
 	do
@@ -53,11 +62,11 @@ uint32_t x;
 			Util::panic();
 	}
 	while (!s.contains("<<<end>>>"));
-	qDebug() << s;
+	if (BLACKSTIRKE_DEBUG) qDebug() << s;
 	s.replace('\n', "");
 	if (rx.indexIn(s) == -1)
 		Util::panic();
-	qDebug() << "string recognized: " << rx.cap();
+	if (BLACKSTIRKE_DEBUG) qDebug() << "string recognized: " << rx.cap();
 	s = rx.cap(1);
 	rx.setPattern("\\s*(\\S+)");
 	if (rx.indexIn(s) == -1)
@@ -65,7 +74,8 @@ uint32_t x;
 	x = rx.cap(1).toUInt(& ok, 16);
 	if (!ok)
 		Util::panic();
-	qDebug() << "read value" << x;
+	if (BLACKSTIRKE_DEBUG) qDebug() << "read value" << x;
+	qDebug() << "target word read took" << t.elapsed() << "milliseconds";
 	return x;
 }
 
@@ -84,6 +94,9 @@ QString s;
 QRegExp rx("<<<start>>>(.*)<<<end>>>");
 bool ok;
 uint32_t x;
+QTime t;
+
+	t.start();
 
 	registers.clear();
 	port->write("base @ >r hex .( <<<start>>>) step u. .( <<<end>>>) r> base ! cr\n");
@@ -99,7 +112,7 @@ uint32_t x;
 	s.replace('\n', "");
 	if (rx.indexIn(s) == -1)
 		Util::panic();
-	qDebug() << "string recognized: " << rx.cap();
+	if (BLACKSTIRKE_DEBUG) qDebug() << "string recognized: " << rx.cap();
 	s = rx.cap(1);
 	rx.setPattern("\\s*(\\S+)");
 	if (rx.indexIn(s) == -1)
@@ -107,6 +120,7 @@ uint32_t x;
 	x = rx.cap(1).toUInt(& ok, 16);
 	if (!ok)
 		Util::panic();
-	qDebug() << "target halt reason: " << x;
+	if (BLACKSTIRKE_DEBUG) qDebug() << "target halt reason: " << x;
+	qDebug() << "target single-stepping took" << t.elapsed() << "milliseconds";
 	return x;
 }
