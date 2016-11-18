6700d79309a3c4f4eaa30c49e59982b3de341823
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-11-18 14:21:54 +0200

    user interface adjustments

--- mainwindow.cxx
+++ mainwindow.cxx
@@ -383,7 +383,7 @@ uint32_t pc(ui->tableWidgetBacktrace->item(row, 0)->text().remove(0, 1).toUInt(0
 	auto locals = dwdata->localDataObjectsForContext(context);
 	for (i = 0; i < locals.size(); ui->tableWidgetLocalVariables->insertRow(row = ui->tableWidgetLocalVariables->rowCount()),
 			ui->tableWidgetLocalVariables->setItem(row, 0, new QTableWidgetItem(QString::fromStdString(dwdata->nameOfDie(locals.at(i))))),
-			ui->tableWidgetLocalVariables->setItem(row, 1, new QTableWidgetItem(QString::fromStdString(dwdata->locationSforthCode(locals.at(i ++), context.at(0), pc)))));
+			ui->tableWidgetLocalVariables->setItem(row, 2, new QTableWidgetItem(QString::fromStdString(dwdata->locationSforthCode(locals.at(i ++), context.at(0), pc)))));
 	qDebug() << "local data objects view built in " << x.elapsed() << "milliseconds";
 }
 
