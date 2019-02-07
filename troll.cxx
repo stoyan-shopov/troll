2af408162f53a201f3540d19ebf2ef1e28df514e
Stoyan Shopov <stoyan.shopov@gmail.com>
2019-02-07 14:45:35 +0200

    Update copyright years for some files

--- troll.cxx
+++ troll.cxx
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2016-2017 stoyan shopov
+Copyright (c) 2016-2017, 2019 Stoyan Shopov
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
